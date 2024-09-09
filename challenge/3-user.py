06d581c7c7ef47a559c53a6f133634924503ee2d
Silver <silverdontask@SilverDontAsk>
2024-09-09 14:39:05 -0800

    The issue in this code seems to have been a not statement added during the test case section, causing it so that if the user password is a valid password to fail because its valid, that __password was not set to private and the is_valid_password function was searching for upper rather than the intended lower

--- challenge/3-user.py
+++ challenge/3-user.py
@@ -13,7 +13,6 @@ class User():
     - password: private string hash in MD5
     """
 
-    __password = None
 
     def __init__(self):
         """
@@ -21,7 +20,7 @@ class User():
         - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
-        self.__password = None
+        self._password = None
 
     @property
     def password(self):
