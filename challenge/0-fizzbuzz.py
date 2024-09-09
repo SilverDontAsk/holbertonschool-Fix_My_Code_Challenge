73636403fbb8059e0efa13052da2795d0e7c12ab
Silver <silverdontask@SilverDontAsk>
2024-09-09 13:03:08 -0800

    >

---
+++ challenge/0-fizzbuzz.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" FizzBuzz
+The issue with this code was in the order the process of
+fizzbuzzing was.
+
+Instead of having it be first searching for what to
+fizz, we must first search for what to fizzbuzz
+as if we fizz first it'll lead to issues where
+numbers that are fizzbuzz turn into fizz. Same would go
+for if buzz was put as the first if statement.
+"""
+import sys
+
+
+def fizzbuzz(n):
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
---
+++ challenge/README.md
@@ -0,0 +1,1 @@
+These are fix my code challenges provided by holberton school
